7034e316382e469da73448752fcb1edb97835c18
Balthier <rrodrijose@gmail.com>
2022-08-03 18:31:47 -0500

    delete and edit btn

--- index.js
+++ index.js
@@ -2,14 +2,17 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const methodOverride = require('method-override')
 
-app.use(express.urlencoded({ extended: true }))
+
 
 // Express Settings
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app. use(methodOverride('_method'))
 
 // Controllers & Routes
 app.use('/places', require('./controllers/places'))
